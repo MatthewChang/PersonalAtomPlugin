df83a2a0f1cc5e77973ffdfa0c3698434b5d2d18
Matthew Chang <m_chang@mit.edu>
2016-10-30 12:55:33 -0400

    initla commit

---
+++ spec/wordcount-test-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WordcountTestView from '../lib/wordcount-test-view';
+
+describe('WordcountTestView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
